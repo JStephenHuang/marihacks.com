50d4f1fac6712dd13ab7e97702e371d52e5f407c
JStephenHuang <jstephhuang@gmail.com>
2023-12-23 10:55:50 -0500

    first commit

---
+++ src/components/navbar.tsx
@@ -0,0 +1,11 @@
+import "../css/navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul></ul>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/themedButtons.tsx
@@ -0,0 +1,3 @@
+const ThemedButtons = () => {
+  return;
+};
